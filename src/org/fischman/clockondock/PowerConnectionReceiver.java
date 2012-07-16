ef1fb010afca4e8313cf498ae7ecc0188ed97ceb
Ami Fischman <fischman@chromium.org>
2012-07-15 21:03:50 -0700

    Plugging in USB power launches clock on GalaxyNexus running JellyBean!

---
+++ src/org/fischman/clockondock/PowerConnectionReceiver.java
@@ -0,0 +1,20 @@
+package org.fischman.clockondock;
+
+import android.app.UiModeManager;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+
+public class PowerConnectionReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) { 
+    	UiModeManager modeManager = (UiModeManager)context.getSystemService(Context.UI_MODE_SERVICE);
+    	modeManager.setNightMode(UiModeManager.MODE_NIGHT_YES);
+    	Intent clockIntent = context.getPackageManager().getLaunchIntentForPackage("com.google.android.deskclock");  // JB on GN.
+    	if (clockIntent == null)
+    		clockIntent = context.getPackageManager().getLaunchIntentForPackage("com.android.deskclock");  // Pre-JB?  Definitely for emulator.
+    	clockIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+    	context.startActivity(clockIntent);
+    }
+}
