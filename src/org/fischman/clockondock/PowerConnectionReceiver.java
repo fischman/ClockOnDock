159c1299cdcd94a4397cc03f531c58a0414b81b5
Ami Fischman <fischman@chromium.org>
2012-07-17 00:27:29 -0700

    Try various package names for different phones/images.

--- src/org/fischman/clockondock/PowerConnectionReceiver.java
+++ src/org/fischman/clockondock/PowerConnectionReceiver.java
@@ -3,14 +3,29 @@ package org.fischman.clockondock;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 public class PowerConnectionReceiver extends BroadcastReceiver {
-    @Override
-    public void onReceive(Context context, Intent intent) { 
-    	Intent clockIntent = context.getPackageManager().getLaunchIntentForPackage("com.google.android.deskclock");  // JB on GN.
-    	if (clockIntent == null)
-    		clockIntent = context.getPackageManager().getLaunchIntentForPackage("com.android.deskclock");  // Pre-JB?  Definitely for emulator.
-    	clockIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-    	context.startActivity(clockIntent);
+  @Override
+  public void onReceive(Context context, Intent intent) {
+    String[] packageNames = {
+        "com.google.android.deskclock", // JB on GN
+        "com.android.deskclock", // Emulator, possibly pre-Froyo
+        "com.htc.android.worldclock", // HTC
+        "com.motorola.blur.alarmclock", // Moto Blur
+        "com.sec.android.app.clockpackage", // Samsung Galaxy
+    };
+    Intent clockIntent = null;
+    for (String packageName : packageNames) {
+      clockIntent = context.getPackageManager().getLaunchIntentForPackage(packageName);
+      if (clockIntent != null)
+        break;
     }
+    if (clockIntent != null) {
+      clockIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+      context.startActivity(clockIntent);
+    } else {
+      Log.w("ClockOnDock", "Couldn't find a launchable intent for any package name!");
+    }
+  }
 }
